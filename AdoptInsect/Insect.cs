4afe4c854e815424d78a9a5a9170ac7e242582ba
Void <Void@DESKTOP-G4T0EEL>
2023-04-05 12:57:14 -0500

    Add project files.

---
+++ AdoptInsect/Insect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptInsect
+{
+    internal class Insect
+    {
+        // Attributes
+        public string ScientificName;
+        public string Nickname;
+
+        // Methods
+        public Insect()
+        {
+            // contructor
+        }
+
+        public void Speak()
+        {
+            //Do some Speaking
+            Console.WriteLine("Hello");
+        }
+
+        public string BetterSpeak()
+        {
+            //Do some Speaking
+            return "Hello";
+        }
+    }
+}
---
+++ AdoptInsect/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AdoptInsect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an insect
+            Insect MyInsect = new Insect();
+
+            MyInsect.Speak();
+
+            Console.WriteLine(MyInsect.Speak());
+            Console.ReadLine();
+        }
+    }
+}
